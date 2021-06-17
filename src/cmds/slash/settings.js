9b8f6cfac5e92ccd3b14d2aee602c5737eed7551
c0reme <52957759+c0reme@users.noreply.github.com>
2021-06-17 17:17:19 +0100

    Update settings.js

--- src/cmds/slash/settings.js
+++ src/cmds/slash/settings.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { isNil } = require('lodash');
+let { db, json } = require('../../config');
 
 module.exports = {
     name: 'settings',
@@ -42,6 +42,53 @@ module.exports = {
      */
     run: async (interaction, client) => {
         const data = interaction.data;
-        
+        const key = interaction.guild_id;
+
+        let setting = data.options[0].value;
+        const value = data.options[1].value;
+        await db.guild.get(key);
+        if (['webhooks'].includes(setting)) {
+            setting = setting[0].toUpperCase() + setting.slice(1);
+            let data = await db.guild.get(`${key}.webhooks`);
+            if (typeof value !== 'boolean') return {
+                embeds: [
+                    new Discord.MessageEmbed()
+                        .setColor(json.colors.err)
+                        .setDescription(`Value is not type boolean`)
+                ]
+            }
+            if (data === value) {
+                if (value === false) return {
+                    embeds: [
+                        new Discord.MessageEmbed()
+                            .setColor(json.colors.failed)
+                            .setDescription(`<:x_:852461875434225674> Failed, **${setting}** are already disabled!`)
+                    ]
+                }
+                return {
+                    embeds: [
+                        new Discord.MessageEmbed()
+                            .setColor(json.colors.failed)
+                            .setDescription(`<:x_:852461875434225674> Failed, **${setting}** are already enabled!`)
+                    ]
+                }
+            }
+            await db.guild.set(`${key}.webhooks`, value);
+            console.log(await db.guild.get(`${key}.webhooks`), value)
+            if (value === false) return {
+                embeds: [
+                    new Discord.MessageEmbed()
+                        .setColor(json.colors.failed)
+                        .setDescription(`<:x_:852461875434225674> **${setting}** has been disabled successfully!`)
+                ]
+            }
+            return {
+                embeds: [
+                    new Discord.MessageEmbed()
+                        .setColor(json.colors.def)
+                        .setDescription(`<:tick_:852461875563855872> **${setting}** has been enabled successfully!`)
+                ]
+            }
+        }
     }
 }
